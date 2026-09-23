2d853958f89e9182b299fa82e30c21e1c4a45334
salahunovmarsel-ops <salahunovmarsel@gmail.com>
2026-09-23 13:53:20 +0600

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -4,5 +4,5 @@
    Это НЕ секрет: anon-ключ предназначен для сайта.
    НЕ вставляйте сюда service_role / secret ключ и пароль базы!
    ============================================================ */
-window.SUPABASE_URL = "";       // напр. https://abcdefgh.supabase.co
-window.SUPABASE_ANON_KEY = "";  // длинная строка anon / public
+window.SUPABASE_URL = "https://trseyokpeuzyxovxqrul.supabase.co";       // напр. https://abcdefgh.supabase.co
+window.SUPABASE_ANON_KEY = "sb_publishable_WgyD3XaoZcIu-3hB-ud7TA_tj9jA7a_";  // длинная строка anon / public
